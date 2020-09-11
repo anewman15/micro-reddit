b0549abb06f62755db31ace2a252638a5d482e50
Abdullah Numan <anewman15@hotmail.com>
2020-09-11 18:09:58 +0600

    Create Post model, add migration, validation and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: {in: 5..15}
   validates :email, presence: true, uniqueness: true, confirmation: true
+
+  has_many :posts
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_144800) do
+ActiveRecord::Schema.define(version: 2020_09_11_104727) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment_body"
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -19,4 +38,7 @@ ActiveRecord::Schema.define(version: 2020_09_10_144800) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
